f1369beb75b9a43cb269476e1e92351337fc8f9e
Andriano <anditoazza@gmail.com>
2022-04-13 10:14:26 -0300

    Atividade Pilha LSE
    Conclusão da atividade de pilhas encadeadas.

---
+++ atividade_lse/master.py
@@ -0,0 +1,25 @@
+from lse import *
+
+#push pop peek __len__
+class PilhaEncadeada:
+    def __init__(self):
+        self.lista = LSE()
+    
+    def __len__(self):
+        return len(self.lista)
+
+    def push(self, item):
+        if self.lista.head is None:
+            self.lista.inserir_inicio(Nodo(item))
+        
+    def pop(self):
+        if len(self) > 0:
+            return self.lista.remover_inicio()
+        else:
+            return None
+
+    def peek(self):
+        if self.lista.head is None:
+            return None
+        else:
+            return self.lista.head.dado
